2fa184e3345636b75882078f05f0ac7068b99247
CryoMilo <minkojee@gmail.com>
2024-12-02 02:09:51 +0700

    added bento grid

---
+++ src/app/components/Projects/Projects.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
+import {
+	TbClipboardCopy,
+	TbFileBroken,
+	TbSignature,
+	TbTableColumn,
+} from "react-icons/tb";
+
+export function Projects() {
+	return (
+		<BentoGrid className="container">
+			{items.map((item, i) => (
+				<BentoGridItem
+					key={i}
+					title={item.title}
+					description={item.description}
+					header={item.header}
+					className={item.className}
+					icon={item.icon}
+				/>
+			))}
+		</BentoGrid>
+	);
+}
+const Skeleton = () => (
+	<div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl [mask-image:radial-gradient(ellipse_at_center,white,transparent)] border border-transparent bg-neutral-100"></div>
+);
+const items = [
+	{
+		title: "The Dawn of Innovation",
+		description: "Explore the birth of groundbreaking ideas and inventions.",
+		header: <Skeleton />,
+		className: "md:col-span-2",
+		icon: <TbClipboardCopy className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Digital Revolution",
+		description: "Dive into the transformative power of technology.",
+		header: <Skeleton />,
+		className: "md:col-span-1",
+		icon: <TbFileBroken className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Art of Design",
+		description: "Discover the beauty of thoughtful and functional design.",
+		header: <Skeleton />,
+		className: "md:col-span-1",
+		icon: <TbSignature className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Power of Communication",
+		description:
+			"Understand the impact of effective communication in our lives.",
+		header: <Skeleton />,
+		className: "md:col-span-2",
+		icon: <TbTableColumn className="h-4 w-4 text-neutral-500" />,
+	},
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero/Hero";
+import { Projects } from "./components/Projects/Projects";
 
 export default function Home() {
 	return (
 		<main>
 			<Hero />
+			<Projects />
 		</main>
 	);
 }
