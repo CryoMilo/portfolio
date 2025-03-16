6abd5f98a1d0e445af7ebcb4e40f31ac864a557b
Damian Aung <damianaung@Damians-MacBook-Air.local>
2025-03-17 01:15:18 +0700

    added resume download

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
+"use client";
+
 import { PiDownloadSimpleThin } from "react-icons/pi";
 
 const Navbar = () => {
+	const handleDownload = () => {
+		const resumePath = "/documents/Oak_Soe_Htoo_Aung_Resume.pdf";
+		const link = document.createElement("a");
+		link.href = resumePath;
+		link.download = "Oak_Soe_Htoo_Aung_Resume.pdf";
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
+
 	return (
 		<nav className="container my-4 flex justify-end items-center">
 			<div className="flex gap-2 items-center">
-				<button>Resume</button>
-				<PiDownloadSimpleThin className="text-xl" />
+				<button onClick={handleDownload} className="flex items-center gap-1">
+					Resume
+					<PiDownloadSimpleThin className="text-xl" />
+				</button>
 			</div>
 		</nav>
 	);
